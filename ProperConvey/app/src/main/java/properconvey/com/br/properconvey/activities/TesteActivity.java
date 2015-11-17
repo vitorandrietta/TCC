72d975b4d99333ec183788c00ce7e42665840dec
Vitor Andrietta <vitorandrietta@gmail.com>
2015-11-16 22:05:40 -0200

    escrita de audio no Gdrive

--- ProperConvey/app/src/main/java/properconvey/com/br/properconvey/activities/TesteActivity.java
+++ ProperConvey/app/src/main/java/properconvey/com/br/properconvey/activities/TesteActivity.java
@@ -1,7 +1,11 @@
 package properconvey.com.br.properconvey.activities;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
@@ -15,20 +19,30 @@ import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.events.CompletionEvent;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 import properconvey.com.br.properconvey.R;
 
-public class TesteActivity extends AppCompatActivity implements RecognitionListener {
+public class TesteActivity extends AppCompatActivity implements RecognitionListener, GoogleApiClient.ConnectionCallbacks , GoogleApiClient.OnConnectionFailedListener{
+    private InputStream filleStream;
     private SpeechRecognizer fala = null;
     private Intent recognizerIntent;
     private String LOG_TAG = "VoiceRecognitionActivity";
     private GoogleApiClient mGoogleApiClient;
+    private final static int RESOLVE_CONNECTION_REQUEST_CODE =562;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,8 +58,8 @@ public class TesteActivity extends AppCompatActivity implements RecognitionListe
         this.getPackageName());
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
         RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
-        recognizerIntent.putExtra("android.speech.extra.GET_AUDIO_FORMAT", "audio/AMR");
-        recognizerIntent.putExtra("android.speech.extra.GET_AUDIO", true);
+        //recognizerIntent.putExtra("android.speech.extra.GET_AUDIO_FORMAT", "audio/AMR");
+        //recognizerIntent.putExtra("android.speech.extra.GET_AUDIO", true);
 
         /*mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Drive.API)
@@ -54,7 +68,7 @@ public class TesteActivity extends AppCompatActivity implements RecognitionListe
                 .addOnConnectionFailedListener(this)
                 .build();*/
 
-        //recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
         fala.startListening(recognizerIntent);
     }
 
@@ -90,21 +104,112 @@ public class TesteActivity extends AppCompatActivity implements RecognitionListe
 
     @Override
     public void onResults(Bundle results) {
+
         ArrayList<String> matches = results
         .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
         Uri audioUri = recognizerIntent.getData();
         ContentResolver contentResolver = getContentResolver();
-        InputStream filleStream;
+        filleStream = null;
         try {
             filleStream = contentResolver.openInputStream(audioUri);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.print("lalala");
+
+        //Drive.DriveApi.newDriveContents(mGoogleApiClient).;
+
+      /*  if(this.isNetworkAvailable()){
+
+
+
+
+
+            ResultCallback<DriveApi.DriveContentsResult> contentsCallback = new
+                    ResultCallback<DriveApi.DriveContentsResult>() {
+
+
+                        @Override
+                        public void onResult(DriveApi.DriveContentsResult result) {
+                            if (!result.getStatus().isSuccess()) {
+                                // Handle error
+                                return;
+                            }
+
+
+                            DriveContents contents = result.getDriveContents();
+                            OutputStream oos = contents.getOutputStream();
+
+
+                            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                            byte[] b = new byte[1024];
+                            while (true) {
+
+                                int bytesRead = 0;
+                                try {
+                                    bytesRead = filleStream.read(b);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                if(bytesRead == -1){
+
+                                    break;
+
+                                }
+
+                                try {
+                                    oos.write(b);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                try {
+                                    oos.flush();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                            }
+
+
+                            try {
+                                oos.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+
+                            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                    .setTitle("file"+Math.floor(Math.random()*1000))
+                                    .setMimeType("text/plain").build();
+
+
+
+                            Drive.DriveApi.getRootFolder(mGoogleApiClient)
+                                    .createFile(mGoogleApiClient, changeSet, contents);
+
+                        }
+                    };
+
+            Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                    .setResultCallback(contentsCallback);
+
+
+        }*/
+
+        //System.out.print("lalala");
         fala.startListening(recognizerIntent);
 
     }
 
+
+    /*private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }*/
     @Override
     public void onPartialResults(Bundle partialResults) {
         Log.i(LOG_TAG, "onPartialResults");
@@ -113,5 +218,49 @@ public class TesteActivity extends AppCompatActivity implements RecognitionListe
     @Override
     public void onEvent(int eventType, Bundle params) {
         Log.i(LOG_TAG, "onEvent");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);//constante
+            } catch (IntentSender.SendIntentException e) {
+                // Unable to resolve, message user appropriately
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESOLVE_CONNECTION_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
+
     }
 }
